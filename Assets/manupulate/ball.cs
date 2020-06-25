f87a74d0142a92ea0526ec04d3c3f9e23b92468e
DESKTOP-VIJKAJF\lesse <lesseraroma@outlook.jp>
2020-06-25 23:47:23 +0900

    適当な挙動を追加

---
+++ Assets/manupulate/ball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            pos -= transform.forward * 0.5f;
+        }else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            pos += transform.forward * 0.5f;
+        }else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            pos -= transform.right * 0.5f;
+        }else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            pos += transform.right * 0.5f;
+        }
+        transform.position = pos;
+    }
+}
---
+++ Assets/manupulate/box.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class box : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Update collision
+    void onCollisionEnter(Collision col)
+    {
+        Destroy(this.gameObject);
+    }
+}
